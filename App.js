e3684c05d64c94b2e5fde387e6c760eac648326b
zephyrus21 <pandey.piyus@gmail.com>
2020-11-12 00:02:02 +0530

    up1

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './store/reducers/productsReducer';
+
+const rootReducer = combineReducers({
+    product: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
     return (
-        <View style={styles.container}>
+        <Provider store={store}>
             <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        </Provider>
     );
 }
