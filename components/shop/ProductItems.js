83dcaa9b56403b7277faafbc9531badf0c7543ea
zephyrus21 <pandey.piyus@gmail.com>
2020-11-18 21:32:47 +0530

    adding homeScreen

---
+++ components/shop/ProductItems.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+const ProductItems = (props) => {
+  return (
+    <View style={styles.product}>
+      <Image style={styles.image} source={{ uri: props.image }} />
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.price}>{props.price}</Text>
+      </View>
+      <View style={styles.actions}>
+        <Button title="View Details" onPress={props.onViewDetails} />
+        <Button title="Go to Cart" onPress={props.onAddtoCart} />
+      </View>
+    </View>
+  );
+};
+
+export default ProductItems;
+
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    height: 300,
+    margin: 20,
+    overflow: 'hidden',
+  },
+  image: { width: '100%', height: '60%' },
+  details: { alignItems: 'center', height: '15%', padding: 10 },
+  title: {
+    fontSize: 18,
+    marginVertical: 4,
+  },
+  price: {
+    fontSize: 14,
+    color: '#888',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '25%',
+    paddingHorizontal: 20,
+  },
+});
--- screens/shops/ProductsOverviewScreen.js
+++ screens/shops/ProductsOverviewScreen.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import ProductItems from '../../components/shop/ProductItems';
+
 const ProductsOverviewScreen = () => {
-    const products = useSelector((state) => state.products.availableProducts);
-    return (
-        <FlatList
-            data={products}
-            renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+  const products = useSelector((state) => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      renderItem={(itemData) => (
+        <ProductItems
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetails={() => {}}
+          onAddtoCart={() => {}}
         />
-    );
+      )}
+    />
+  );
 };
 
 export default ProductsOverviewScreen;
